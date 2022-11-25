e36d9d29a886e576cfaeca13ecb8d337a7b0a394
Ed Jones <8016457+ed-jones@users.noreply.github.com>
2022-11-25 14:30:07 +1100

    chore: :construction_worker: fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		dts({
+			insertTypesEntry: true,
+		}),
+		react(),
+	],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: 'Material Icons',
+			fileName: 'index',
+		},
+	},
 });
