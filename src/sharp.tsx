1b3dd7978bf5e30a0e45c8411b24716fbb054a7a
Ed Jones <8016457+ed-jones@users.noreply.github.com>
2023-03-10 18:07:13 +1100

    fix: :arrow_up: update dependencies

--- src/sharp.tsx
+++ src/sharp.tsx
@@ -1,3 +1,11 @@
-import SharpIcon from './icon';
-import './css/sharp.css';
-export default SharpIcon;
+import Icon from './icon';
+import { PolymorphicMaterialSymbolProps } from './icon';
+import classes from './css/sharp.module.css';
+import { ElementType } from 'react';
+import { combineClasses } from './utils';
+
+const Sharp = <C extends ElementType>(props: PolymorphicMaterialSymbolProps<C>) => {
+	return <Icon {...props} className={combineClasses(classes.sharp, props.className)} />;
+};
+
+export default Sharp;
